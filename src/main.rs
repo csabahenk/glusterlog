fbc3d39d09a3a684598b2534a68795ea56361eee
Csaba Henk <csaba@lowlife.hu>
2020-03-23 18:59:40 +0100

    implement matching

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::io;
 use std::io::prelude::*;
 
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
 macro_rules! fatal {
     ($($tt:tt)*) => {{
         use std::io::Write;
@@ -10,14 +15,39 @@ macro_rules! fatal {
 }
 
 
-fn parse(s: &str) {
-    println!("GOT: {}", s);
+lazy_static! {
+    static ref LOG_PATTERN: Regex = Regex::new(r"(?x)
+\A
+\[(?P<ts>[^\]]+)\]
+\s
+(?P<log_level>[CIEWTD])
+\s
+(?:\[MSGID:\s(?P<msg_id>[^\]]+)\]\s)?
+\[(?P<file_info>[^\]]+)\]
+\s
+(?P<domain>[^:]+):
+\s
+(?P<msg_parts_raw>.+)
+").unwrap();
+}
+
+fn parse(s: &str) -> Option<String> {
+    LOG_PATTERN.captures(s).map(|cap| {
+        format!("FOUND: ts: {}, log_level: {}, msg_id: {}, file_info: {}, domain: {}, \
+                 msg_parts_raw: {}",
+                &cap["ts"],
+                &cap["log_level"],
+                &cap.name("msg_id").map_or("<NONE>", |m| m.as_str()),
+                &cap["file_info"],
+                &cap["domain"],
+                &cap["msg_parts_raw"])
+    })
 }
 
 fn process_lines() -> io::Result<()> {
     let stdin = io::stdin();
-    for line_result in stdin.lock().lines() {
-        parse(&line_result?)
+    for (idx, line_result) in stdin.lock().lines().enumerate() {
+        parse(&line_result?).map(|s| println!("{} {}", idx + 1, s));
     }
     Ok(())
 }

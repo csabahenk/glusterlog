abd55ec433ee2fc003c31b2bc9284077c4676cf6
Csaba Henk <csaba@lowlife.hu>
2020-04-16 15:14:07 +0200

    print all lines in JSON wrapper (unparsable ones too)

--- src/main.rs
+++ src/main.rs
@@ -32,24 +32,33 @@ lazy_static! {
 ").unwrap();
 }
 
-fn parse(s: &str) -> Option<Value> {
-    LOG_PATTERN.captures(s).map(|cap| {
-        let mut obj = Map::new();
-        for name in LOG_PATTERN.capture_names() {
-            if let Some(name) = name {
-                if let Some(value) = cap.name(name) {
-                    obj.insert(name.to_string(), Value::String(value.as_str().to_string()));
+fn parse(s: &str) -> Value {
+    let mut obj = Map::new();
+
+    match LOG_PATTERN.captures(s) {
+        Some(cap) => {
+            obj.insert("known_format".to_string(), Value::Bool(true));
+            for name in LOG_PATTERN.capture_names() {
+                if let Some(name) = name {
+                    if let Some(value) = cap.name(name) {
+                        obj.insert(name.to_string(), Value::String(value.as_str().to_string()));
+                    }
                 }
             }
         }
-        Value::Object(obj)
-    })
+        None => {
+            obj.insert("known_format".to_string(), Value::Bool(false));
+            obj.insert("message".to_string(), Value::String(s.to_string()));
+        }
+    };
+    Value::Object(obj)
 }
 
 fn process_lines() -> io::Result<()> {
     let stdin = io::stdin();
-    for (idx, line_result) in stdin.lock().lines().enumerate() {
-        parse(&line_result?).map(|o| println!("{} {}", idx + 1, o.to_string()));
+    for line_result in stdin.lock().lines() {
+        let o = parse(&line_result?);
+        println!("{}", o.to_string());
     }
     Ok(())
 }
@@ -58,6 +67,4 @@ fn main() {
     if let Err(err) = process_lines() {
         fatal!("processing lines: {}", err)
     }
-
-    println!("Done.");
 }

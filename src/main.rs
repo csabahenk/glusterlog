51d72cfebfb099049133eed5aab1250565f6469d
Csaba Henk <csaba@lowlife.hu>
2020-04-17 15:59:09 +0200

    handle entries of repeated log messages

--- src/main.rs
+++ src/main.rs
@@ -15,64 +15,88 @@ macro_rules! fatal {
     }}
 }
 
+fn make_ts_pattern(s: &str) -> String {
+    format!(r"\[(?P<{}>[^\]]+)\]", s)
+}
 
 lazy_static! {
-    static ref LOG_PATTERN: Regex = Regex::new(r"(?x)
-\A
-\[(?P<ts>[^\]]+)\]
-\s
-(?P<log_level>[CIEWTD])
+    static ref LOG_PATTERN_STRING: &'static str = r"(?P<log_level>[CIEWTD])
 \s
 (?:\[MSGID:\s(?P<msg_id>[^\]]+)\]\s)?
 \[(?P<file_info>[^\]]+)\]
 \s
 (?P<domain>[^:]+):
 \s
-(?P<msg_parts_raw>.+)
-").unwrap();
+(?P<msg_parts_raw>.+)";
+
+    static ref LOG_PATTERN: Regex = Regex::new(&format!(r"(?x)\A{}\s{}",
+        make_ts_pattern("ts"),
+        *LOG_PATTERN_STRING)).unwrap();
+
+    static ref LOG_REPEAT_PATTERN: Regex = Regex::new(&format!(
+        r#"(?x)\AThe\ message\ "{}"\ repeated\ (?P<repetitions>\d+)\ times\ between\ {}\ and\ {}\z"#,
+        *LOG_PATTERN_STRING,
+        make_ts_pattern("ts_beg"),
+        make_ts_pattern("ts_end"))).unwrap();
 }
 
 fn parse(s: &str) -> Value {
     let mut obj = Map::new();
 
-    match LOG_PATTERN.captures(s) {
-        Some(cap) => {
+    match match LOG_PATTERN.captures(s) {
+        Some(cap) => (&*LOG_PATTERN, Some(cap)),
+        None => (&*LOG_REPEAT_PATTERN, LOG_REPEAT_PATTERN.captures(s)),
+    } {
+        (rx, Some(cap)) => {
             obj.insert("known_format".to_string(), Value::Bool(true));
-            for name in LOG_PATTERN.capture_names() {
+            for name in rx.capture_names() {
                 if let Some(name) = name {
                     if let Some(value) = cap.name(name) {
-                        // "msg_parts_raw needs" postprocessing
-                        if name == "msg_parts_raw" {
-                            // ... split it with tabs
-                            let mut splitter = value.as_str().split('\t');
-                            if let Some(msg) = splitter.next() {
-                                // first entry is value for "meesage" key
-                                obj.insert("message".to_string(), Value::String(msg.to_string()));
-                                // consecutive entries are 'key=val' pairs, which
-                                // are collected under "fields" key
-                                let mut fields = Map::new();
-                                for msg_part in splitter {
-                                    match msg_part.splitn(2, '=').collect::<Vec<&str>>()[..] {
-                                        [key, value] => {
-                                            fields.insert(key.to_string(),
-                                                          Value::String(value.to_string()))
-                                        }
-                                        [key] => fields.insert(key.to_string(), Value::Null),
-                                        _ => panic!("splitn(2,..) result arity not 1 or 2"),
-                                    };
-                                }
-                                if !fields.is_empty() {
-                                    obj.insert("fields".to_string(), Value::Object(fields));
+                        match name {
+                            // "msg_parts_raw needs" postprocessing
+                            "msg_parts_raw" => {
+                                // ... split it with tabs
+                                let mut splitter = value.as_str().split('\t');
+                                if let Some(msg) = splitter.next() {
+                                    // first entry is value for "meesage" key
+                                    obj.insert("message".to_string(),
+                                               Value::String(msg.to_string()));
+                                    // consecutive entries are 'key=val' pairs, which
+                                    // are collected under "fields" key
+                                    let mut fields = Map::new();
+                                    for msg_part in splitter {
+                                        match msg_part.splitn(2, '=').collect::<Vec<&str>>()[..] {
+                                            [key, value] => {
+                                                fields.insert(key.to_string(),
+                                                              Value::String(value.to_string()))
+                                            }
+                                            [key] => fields.insert(key.to_string(), Value::Null),
+                                            _ => panic!("splitn(2,..) result arity not 1 or 2"),
+                                        };
+                                    }
+                                    if !fields.is_empty() {
+                                        obj.insert("fields".to_string(), Value::Object(fields));
+                                    }
                                 }
                             }
-                        } else {
-                            obj.insert(name.to_string(), Value::String(value.as_str().to_string()));
+                            "repetitions" => {
+                                let value = value.as_str();
+                                obj.insert(name.to_string(),
+                                           match value.parse() {
+                                               Ok(n) => Value::Number(n),
+                                               _ => Value::String(value.to_string()),
+                                           });
+                            }
+                            _ => {
+                                obj.insert(name.to_string(),
+                                           Value::String(value.as_str().to_string()));
+                            }
                         }
                     }
                 }
             }
         }
-        None => {
+        (_, None) => {
             obj.insert("known_format".to_string(), Value::Bool(false));
             obj.insert("message".to_string(), Value::String(s.to_string()));
         }
